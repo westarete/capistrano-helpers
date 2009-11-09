146b612ed657f00dbbfa91094a9f678dc2b1da6d
Clinton Judy <clinton@westarete.com>
2009-11-09 11:50:02 -0500

    Setting rails_env to a default of 'production'.

--- lib/capistrano-helpers/gems.rb
+++ lib/capistrano-helpers/gems.rb
@@ -5,6 +5,7 @@ CapistranoHelpers.with_configuration do
   namespace :gems do
     desc "Install gems on the remote server."
     task :install, :roles => :app do
+      rails_env = fetch(:rails_env, "production")
       run "cd #{current_release} && #{sudo} rake gems:install RAILS_ENV=#{rails_env}"
     end
   end
