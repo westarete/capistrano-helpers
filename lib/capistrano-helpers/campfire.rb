394e24856128ad8cab8c27266352945dcc5f02b5
Clinton Judy <clinton@westarete.com>
2010-07-30 11:51:42 -0400

    Update version of tinder.

--- lib/capistrano-helpers/campfire.rb
+++ lib/capistrano-helpers/campfire.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/../capistrano-helpers' if ! defined?(Capistra
 
 begin
   # Campfire API gem for deploy notifications.
+  gem 'tinder', '>= 1.4.0'
   require 'tinder'
 rescue LoadError
   abort "The campfire helper requires the tinder gem. Install it with: sudo gem install tinder"
@@ -18,8 +19,7 @@ CapistranoHelpers.with_configuration do
       username = `whoami`.chomp
       config_file = fetch(:campfire_config, 'config/campfire.yml')
       config = YAML::load_file(config_file)
-      campfire = Tinder::Campfire.new(config['account'])
-      campfire.login(config['email'], config['password'])
+      campfire = Tinder::Campfire.new(config['account'], :username => config['email'], :password => config['password'])
       room = campfire.find_room_by_name(config['room'])
       room.speak("#{username} just deployed #{application} #{branch} to #{stage}")
       room.leave
