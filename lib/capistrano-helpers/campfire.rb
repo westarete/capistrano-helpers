a02c258045cab0181dc729f3d8e774a121f2962d
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2012-05-09 15:08:51 +0200

    Fix Campfire notifications
    Previous version was forcing user to quit and enter room
    This version can be refactored, but at least, it's working.

--- lib/capistrano-helpers/campfire.rb
+++ lib/capistrano-helpers/campfire.rb
@@ -4,10 +4,10 @@ require 'tinder'
 require 'git'
 
 CapistranoHelpers.with_configuration do
-  
+
   namespace :deploy do
     desc 'Make a post to campfire to tell everyone about this deployment.'
-    task :post_to_campfire do
+    task :post_to_campfire_before do
       config_file = [fetch(:campfire_config, 'config/campfire.yml'), "#{ENV['HOME']}/.campfire.yml"].detect { |f| File.readable?(f) }
       if config_file.nil?
          puts "Could not find a campfire configuration. Skipping campfire notification."
@@ -21,11 +21,29 @@ CapistranoHelpers.with_configuration do
         config = YAML::load_file(config_file)
         campfire = Tinder::Campfire.new(config['subdomain'], :token => config['token'])
         room = campfire.find_room_by_name(config['room'])
-        room.speak("#{someone} just deployed #{application} #{branch} to #{target}")
-        room.leave
+        room.speak("#{someone} started deploying #{application} #{branch} to #{target}")
+      end
+    end
+
+    task :post_to_campfire_after do
+      config_file = [fetch(:campfire_config, 'config/campfire.yml'), "#{ENV['HOME']}/.campfire.yml"].detect { |f| File.readable?(f) }
+      if config_file.nil?
+         puts "Could not find a campfire configuration. Skipping campfire notification."
+      else
+        if ! exists?(:application)
+          puts "You should set :application to the name of this app."
+        end
+        git_config = Git.open('.').config rescue {}
+        someone = ENV['GIT_AUTHOR_NAME'] || git_config['user.name'] || `whoami`.strip
+        target = fetch(:stage, 'production')
+        config = YAML::load_file(config_file)
+        campfire = Tinder::Campfire.new(config['subdomain'], :token => config['token'])
+        room = campfire.find_room_by_name(config['room'])
+        room.speak("#{someone} finished deploying #{application} #{branch} to #{target}")
       end
     end
   end
-  after "deploy:restart", "deploy:post_to_campfire"
+  after "deploy:restart", "deploy:post_to_campfire_after"
+  before "deploy", "deploy:post_to_campfire_before"
 
 end
