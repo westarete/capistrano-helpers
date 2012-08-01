970ef52ad667f0af559207c1a449728711c00ae1
Hanfei Shen <qqshfox@gmail.com>
2012-08-01 11:21:50 +0800

    Create a private directory in shared after setup

--- lib/capistrano-helpers/privates.rb
+++ lib/capistrano-helpers/privates.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../capistrano-helpers' if ! defined?(CapistranoHelpers)
 
 CapistranoHelpers.with_configuration do
- 
+
   namespace :deploy do
     desc 'Replace named files with a symlink to their counterparts in shared/private/'
     task :symlink_privates do
@@ -15,8 +15,17 @@ CapistranoHelpers.with_configuration do
         run "rm -rf #{release_path}/#{path} && ln -nfs #{shared_path}/private/#{path} #{release_path}/#{path}"
       end
     end
+
+    desc '[internal] Create a private directory in shared'
+    task :create_private_dir do
+      private_dir = 'private'
+      private_path = File.join(shared_path, private_dir)
+      run "#{try_sudo} mkdir -p #{private_path}"
+      run "#{try_sudo} chmod o-rwx #{private_path}"
+    end
   end
 
   before "deploy:finalize_update", "deploy:symlink_privates"
+  after "deploy:setup", "deploy:create_private_dir"
 
 end
