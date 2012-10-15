416ad3d2e618448a951f0d0067cd40048b4ded9d
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2012-10-15 23:36:47 +0200

    Preserve deploy namespace
    Probably safer like this, we don't override eventual changes in
    deploy:restart
    Filter execution on :no_release

--- lib/capistrano-helpers/unicorn.rb
+++ lib/capistrano-helpers/unicorn.rb
@@ -2,11 +2,12 @@ require File.dirname(__FILE__) + '/../capistrano-helpers' if ! defined?(Capistra
 
 CapistranoHelpers.with_configuration do
 
-  namespace :deploy do
+  namespace :unicorn do
     desc "Restart unicorn"
-    task :restart, :roles => :web do
+    task :restart, :roles => :web, :except => { :no_release => true } do
       run "oldpid=$(cat /var/www/#{application}/#{stage}/shared/pids/unicorn.pid) && kill -s USR2 $oldpid && echo 'Searching for newly spawned master process...' && until (pid=$(cat /var/www/#{application}/#{stage}/shared/pids/unicorn.pid 2>/dev/null) && test '$pid' != '$oldpid' && ps x |grep $pid|grep master) ; do sleep 1 ; done && kill -s WINCH $oldpid && kill -s QUIT $oldpid"
     end
   end
+  after "deploy:restart", "unicorn:restart"
 
 end
